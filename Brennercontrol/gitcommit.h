1dc6eca219d1ba5a5791929aafbf23804bfc4c6e
matthias <matthias@legolas>
2024-10-09 21:08:27 +0200

    test

---
+++ Brennercontrol/gitcommit.h
@@ -0,0 +1,7 @@
+#define GIT_INFO_PRESENT
+ static const char* GIT_INFO = "Version Information=[6c7c0eb67cef3d760c3f4b7c02b9eca9fb84c426, (HEAD -> main, origin/main)]\r\n"; 
+ static const char* GIT_HASH = "6c7c0eb67cef3d760c3f4b7c02b9eca9fb84c426"; 
+ static const byte GIT_UNCOMMITTED_BYTE = 1; 
+
+ static const char* GIT_ID = "6c7c0eb";
+
